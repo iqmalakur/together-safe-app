626feb643d8daff66aa19dd53cda4c5816160063
iqmalakur <iakbarkurnia@gmail.com>
2025-02-16 22:36:28 +0700

    feat: create class object map configuration

---
+++ app/src/main/java/com/togethersafe/app/utils/MapConfig.kt
@@ -0,0 +1,10 @@
+package com.togethersafe.app.utils
+
+object MapConfig {
+    const val ZOOM_DEFAULT = 15.0
+    const val ZOOM_MIN = 5.0
+    const val ZOOM_MAX = 20.0
+    const val ZOOM_STEP = 1.0
+    const val PITCH = 0.0
+    const val BEARING = 0.0
+}
