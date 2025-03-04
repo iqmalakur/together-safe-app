7683888834161575e84ce7fad7b21dfdd88efbaf
iqmalakur <iakbarkurnia@gmail.com>
2025-03-04 08:57:57 +0700

    feat: add unfocused search bar text field when click map

--- app/src/main/java/com/togethersafe/app/ui/view/MapScreen.kt
+++ app/src/main/java/com/togethersafe/app/ui/view/MapScreen.kt
@@ -1,6 +1,7 @@
 package com.togethersafe.app.ui.view
 
 import android.content.Context
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -12,6 +13,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mapbox.android.gestures.MoveGestureDetector
 import com.mapbox.android.gestures.StandardScaleGestureDetector
@@ -170,10 +172,15 @@ private fun Map(
     val userPosition by mapViewModel.userPosition.collectAsState()
     var incidentData by remember { mutableStateOf<Incident?>(null) }
     var isSheetOpen by rememberSaveable { mutableStateOf(false) }
+    val focusManager = LocalFocusManager.current
 
     MapboxMap(
         modifier = Modifier.fillMaxSize(),
         mapViewportState = mapViewportState,
+        onMapClickListener = {
+            focusManager.clearFocus()
+            false
+        },
         scaleBar = { },
         compass = { /* TODO: Pindahkan compass ke bawah kanan */ },
         style = { MapStyle(Style.OUTDOORS) }
