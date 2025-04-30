0a6fddefe89a8b46fb24576c1c6f1d624ca15eab
iqmalakur <iakbarkurnia@gmail.com>
2025-04-30 07:02:29 +0700

    feat: handle upvote and downvote

--- app/src/main/java/com/togethersafe/app/viewmodels/ReportInteractionViewModel.kt
+++ app/src/main/java/com/togethersafe/app/viewmodels/ReportInteractionViewModel.kt
@@ -1,4 +1,64 @@
 package com.togethersafe.app.viewmodels
 
-class ReportInteractionViewModel {
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.togethersafe.app.repositories.ReportInteractionRepository
+import com.togethersafe.app.utils.ApiErrorCallback
+import com.togethersafe.app.utils.handleApiError
+import com.togethersafe.app.utils.withToken
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ReportInteractionViewModel @Inject constructor(
+    private val repository: ReportInteractionRepository,
+    @ApplicationContext private val context: Context,
+) : ViewModel() {
+
+    fun findUserVote(
+        reportId: String,
+        onError: ApiErrorCallback,
+        onSuccess: (voteType: String?) -> Unit
+    ) {
+        viewModelScope.launch {
+            try {
+                withToken(context, onError) { token ->
+                    val result = repository.findUserVote(token, reportId)
+                    onSuccess(result.type)
+                }
+            } catch (e: Exception) {
+                handleApiError(this::class, e, onError)
+            }
+        }
+    }
+
+    fun vote(
+        reportId: String,
+        prevVoteType: String?,
+        targetVoteType: String?,
+        onError: ApiErrorCallback,
+        onSuccess: (voteType: String?) -> Unit,
+        onComplete: () -> Unit,
+    ) {
+        viewModelScope.launch {
+            try {
+                val voteType = when {
+                    prevVoteType == targetVoteType -> null
+                    else -> targetVoteType
+                }
+
+                withToken(context, onError) { token ->
+                    val result = repository.vote(token, reportId, voteType)
+                    onSuccess(result.type)
+                }
+            } catch (e: Exception) {
+                handleApiError(this::class, e, onError)
+            }
+            onComplete()
+        }
+    }
+
 }
--- app/src/main/java/com/togethersafe/app/views/report/ReportDetailScreen.kt
+++ app/src/main/java/com/togethersafe/app/views/report/ReportDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.togethersafe.app.views.report
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -13,16 +14,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -33,8 +41,11 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.togethersafe.app.components.AppHeader
 import com.togethersafe.app.components.UserProfile
+import com.togethersafe.app.data.dto.ReportResDto
 import com.togethersafe.app.navigation.LocalNavController
 import com.togethersafe.app.utils.getViewModel
+import com.togethersafe.app.viewmodels.AppViewModel
+import com.togethersafe.app.viewmodels.ReportInteractionViewModel
 import com.togethersafe.app.viewmodels.ReportViewModel
 import java.text.SimpleDateFormat
 import java.util.Locale
@@ -42,8 +53,26 @@ import java.util.Locale
 @Composable
 fun ReportDetailScreen() {
     val navController = LocalNavController.current
+    val appViewModel: AppViewModel = getViewModel()
     val reportViewModel: ReportViewModel = getViewModel()
+    val reportInteractionViewModel: ReportInteractionViewModel = getViewModel()
     val report by reportViewModel.report.collectAsState()
+
+    var upvoteCount by remember { mutableIntStateOf(0) }
+    var downvoteCount by remember { mutableIntStateOf(0) }
+    var currentVote by remember { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(report) {
+        if (report != null) {
+            upvoteCount = report!!.upvote
+            downvoteCount = report!!.downvote
+
+            reportInteractionViewModel.findUserVote(
+                report!!.id,
+                onError = { _, messages -> appViewModel.setToastMessage(messages[0]) },
+            ) { currentVote = it }
+        }
+    }
 
     Column(
         modifier = Modifier
@@ -62,127 +91,41 @@ fun ReportDetailScreen() {
                     .padding(16.dp),
                 verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                item {
-                    Text(
-                        text = report.incident.category,
-                        style = MaterialTheme.typography.headlineSmall,
-                        fontWeight = FontWeight.Bold
-                    )
-                    Spacer(modifier = Modifier.height(4.dp))
-                    Text(
-                        text = "${report.date} • ${report.time}",
-                        style = MaterialTheme.typography.bodySmall,
-                        color = Color.Gray
-                    )
-                }
-
-                item {
-                    Row(verticalAlignment = Alignment.CenterVertically) {
-                        UserProfile(
-                            imageModel = report.user.profilePhoto,
-                            size = 48.dp
-                        )
-                        Spacer(modifier = Modifier.width(8.dp))
-                        Column {
-                            Text(text = report.user.name, fontWeight = FontWeight.Bold)
-                            Text(
-                                text = "Reputasi: ${report.user.reputation}",
-                                style = MaterialTheme.typography.bodySmall
-                            )
-                        }
-                    }
-                }
-
-                item {
-                    Text(
-                        text = "Deskripsi:",
-                        fontWeight = FontWeight.SemiBold
-                    )
-                    Text(text = report.description)
-                }
-
-                item {
-                    Text(
-                        text = "Lokasi:",
-                        fontWeight = FontWeight.SemiBold
-                    )
-                    Text(text = report.location)
-                }
-
-                if (report.attachments.isNotEmpty()) {
-                    item {
-                        Text(
-                            text = "Lampiran:",
-                            fontWeight = FontWeight.SemiBold
-                        )
-                        Spacer(Modifier.height(4.dp))
-                        LazyRow(
-                            horizontalArrangement = Arrangement.spacedBy(8.dp)
-                        ) {
-                            items(report.attachments) { attachmentUrl ->
-                                AsyncImage(
-                                    model = attachmentUrl,
-                                    contentDescription = "Lampiran",
-                                    modifier = Modifier
-                                        .size(120.dp)
-                                        .clip(RoundedCornerShape(8.dp)),
-                                    contentScale = ContentScale.Crop
-                                )
+                reportDetailContent(report) {
+                    VoteButton(
+                        type = "upvote",
+                        text = "👍 $upvoteCount",
+                        reportId = report.id,
+                        currentVote = currentVote,
+                    ) {
+                        when (currentVote) {
+                            null -> upvoteCount++
+                            "upvote" -> upvoteCount--
+                            else -> {
+                                upvoteCount++
+                                downvoteCount--
                             }
                         }
+
+                        currentVote = it
                     }
-                }
 
-                item {
-                    Row(
-                        horizontalArrangement = Arrangement.spacedBy(16.dp),
-                        modifier = Modifier.fillMaxWidth()
+                    VoteButton(
+                        type = "downvote",
+                        text = "👎 $downvoteCount",
+                        reportId = report.id,
+                        currentVote = currentVote,
                     ) {
-                        Text("👍 ${report.upvote}")
-                        Text("👎 ${report.downvote}")
-                        Text("Status: ${report.status}")
-                    }
-                }
-
-                item {
-                    Text(
-                        text = "Komentar:",
-                        fontWeight = FontWeight.SemiBold
-                    )
-                }
-
-                items(report.comments) { comment ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(vertical = 8.dp)
-                    ) {
-                        Row(verticalAlignment = Alignment.CenterVertically) {
-                            UserProfile(
-                                imageModel = comment.user.profilePhoto,
-                                size = 36.dp
-                            )
-                            Spacer(modifier = Modifier.width(8.dp))
-                            Column {
-                                Text(
-                                    text = comment.user.name,
-                                    fontWeight = FontWeight.Bold
-                                )
-                                Text(
-                                    text = "Reputasi: ${comment.user.reputation}",
-                                    style = MaterialTheme.typography.bodySmall,
-                                    color = Color.Gray
-                                )
+                        when (currentVote) {
+                            null -> downvoteCount++
+                            "downvote" -> downvoteCount--
+                            else -> {
+                                downvoteCount++
+                                upvoteCount--
                             }
                         }
-                        Spacer(modifier = Modifier.height(4.dp))
-                        Text(text = comment.comment)
-                        Text(
-                            text = "${SimpleDateFormat("dd MMM yyyy, HH:mm", Locale.getDefault()).format(comment.createdAt)}${if (comment.isEdited) " (diedit)" else ""}",
-                            style = MaterialTheme.typography.bodySmall,
-                            color = Color.Gray
-                        )
-                        Divider(modifier = Modifier.padding(vertical = 8.dp))
+
+                        currentVote = it
                     }
                 }
             }
@@ -194,5 +137,177 @@ fun ReportDetailScreen() {
                 Text("Tidak ada data laporan.")
             }
         }
+    }
+}
+
+private fun LazyListScope.reportDetailContent(
+    report: ReportResDto,
+    voteButtons: @Composable () -> Unit
+) {
+    item {
+        Text(
+            text = report.incident.category,
+            style = MaterialTheme.typography.headlineSmall,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = "${report.date} • ${report.time}",
+            style = MaterialTheme.typography.bodySmall,
+            color = Color.Gray
+        )
+    }
+
+    item {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            UserProfile(
+                imageModel = report.user.profilePhoto,
+                size = 48.dp
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(text = report.user.name, fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Reputasi: ${report.user.reputation}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+
+    item {
+        Text(
+            text = "Deskripsi:",
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(text = report.description)
+    }
+
+    item {
+        Text(
+            text = "Lokasi:",
+            fontWeight = FontWeight.SemiBold
+        )
+        Text(text = report.location)
+    }
+
+    if (report.attachments.isNotEmpty()) {
+        item {
+            Text(
+                text = "Lampiran:",
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(Modifier.height(4.dp))
+            LazyRow(
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(report.attachments) { attachmentUrl ->
+                    AsyncImage(
+                        model = attachmentUrl,
+                        contentDescription = "Lampiran",
+                        modifier = Modifier
+                            .size(120.dp)
+                            .clip(RoundedCornerShape(8.dp)),
+                        contentScale = ContentScale.Crop
+                    )
+                }
+            }
+        }
+    }
+
+    item {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            voteButtons()
+            Text("Status: ${report.status}")
+        }
+    }
+
+    item {
+        Text(
+            text = "Komentar:",
+            fontWeight = FontWeight.SemiBold
+        )
+    }
+
+    items(report.comments) { comment ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                UserProfile(
+                    imageModel = comment.user.profilePhoto,
+                    size = 36.dp
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Column {
+                    Text(
+                        text = comment.user.name,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Reputasi: ${comment.user.reputation}",
+                        style = MaterialTheme.typography.bodySmall,
+                        color = Color.Gray
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = comment.comment)
+            Text(
+                text = "${
+                    SimpleDateFormat(
+                        "dd MMM yyyy, HH:mm",
+                        Locale.getDefault()
+                    ).format(comment.createdAt)
+                }${if (comment.isEdited) " (diedit)" else ""}",
+                style = MaterialTheme.typography.bodySmall,
+                color = Color.Gray
+            )
+            HorizontalDivider(modifier = Modifier.padding(vertical = 8.dp))
+        }
+    }
+}
+
+@Composable
+private fun VoteButton(
+    type: String,
+    text: String,
+    reportId: String,
+    currentVote: String?,
+    onSuccessVote: (newVote: String?) -> Unit
+) {
+    val appViewModel: AppViewModel = getViewModel()
+    val reportInteractionViewModel: ReportInteractionViewModel = getViewModel()
+
+    TextButton(
+        colors = ButtonDefaults.textButtonColors(
+            containerColor = when (currentVote) {
+                type -> Color.Gray
+                else -> Color.White
+            },
+            contentColor = when (currentVote) {
+                type -> Color.White
+                else -> Color.Black
+            },
+        ),
+        border = BorderStroke(1.dp, Color.Black.copy(0.2f)),
+        onClick = {
+            appViewModel.setLoading(true)
+            reportInteractionViewModel.vote(
+                reportId = reportId,
+                prevVoteType = currentVote,
+                targetVoteType = type,
+                onError = { _, messages -> appViewModel.setToastMessage(messages[0]) },
+                onSuccess = onSuccessVote
+            ) { appViewModel.setLoading(false) }
+        }
+    ) {
+        Text(text)
     }
 }
